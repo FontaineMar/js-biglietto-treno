58a9f4f09c074faa794d3ec461a4acd08294555c
marco <odri.fo@gmail.com>
2020-10-21 23:25:41 +0200

    bloccato

--- js/script.js
+++ js/script.js
@@ -1,15 +1,70 @@
+// Dati
 
-var nome = document.getElementById("namehtml");
-var km = document.getElementById('kmhtml');
-var discountY = document.getElementById("discyoung");
-var discountO = document.getElementById("discold");
-var normal = document.getElementById("nothing")
 
-km = (km * 0.21);
+// button
+var generetor = document.getElementById("buttonhtml");
 
-if (discountY){
-  Math.to(km * 0.80)
-}
+
+
+
+
+generetor.addEventListener('click', function(){
+
+  // calcolo chilometri
+  var km = document.getElementById("kmHtml");
+  var kmJs = km * 0.21 ;
+
+  // discount
+  var disc = document.getElementById("discount").value;
+
+
+  if (disc == "minorenne"){
+    var price = document.getElementById("cost")
+    price.innerHTML = Math.floor(kmJs * 0.80) + ("€");
+  }
+  else if (disc == "over65"){
+    var price = document.getElementById("cost")
+    price.innerHTML = Math.floor(kmJs * 0.60) + ("€");
+  }
+  else if (disc == "nessuno"){
+    var price = document.getElementById("cost");
+    price.innerHTML = Math.floor(kmJs) + ("€");
+  }
+
+
+
+
+
+
+// CODICE TRENO
+train = document.getElementById("trainNumber");
+numtrain = Math.random(1) * 10000 ;
+train.innerHTML =  (Math.floor(numtrain));
+
+
+// NUMERO CARROZZA
+carrozza = document.getElementById("carriage");
+numCarr = Math.random(1) * 10 + 1 ;
+carrozza.innerHTML =  (Math.floor(numCarr));
+
+
+var disc = document.getElementById("discount").value;
+var discName = document.getElementById("typeDiscount");
+discName.innerHTML = disc.value;
+
+// NOME CLIENTE
+  var name = document.getElementById("firstname");
+  var ticketName = document.getElementById("finalname");
+  ticketName.innerHTML = name.value;
+});
+
+// <select name="cars" id="cars">
+//   <option value="volvo">Volvo</option>
+//   <option value="saab">Saab</option>
+//   <option value="mercedes">Mercedes</option>
+//   <option value="audi">Audi</option>
+// </select>
+
 
 // else if (eta >= 65){
 // document.getElementById("prezzo").innerHTML = ("il prezzo del biglietto è di " ) + Math.floor(km * 0.60) + ("€");
