0e08a7a64859b58c709d9a51c887abc316371b99
marco <odri.fo@gmail.com>
2020-10-21 15:23:31 +0200

    inizio progetto

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-var eta = parseInt(prompt("Quanti hanni hai?"));
-var km = parseInt(prompt("Quanti km percorrerai?"));
+
+var nome = document.getElementById("namehtml");
+var km = document.getElementById('kmhtml');
+var discountY = document.getElementById("discyoung");
+var discountO = document.getElementById("discold");
+var normal = document.getElementById("nothing")
 
 km = (km * 0.21);
 
-if (eta < 18){
-document.getElementById("prezzo").innerHTML = ("il prezzo del biglietto è di " ) + Math.floor(km * 0.80) + ("€");
+if (discountY){
+  Math.to(km * 0.80)
 }
 
-else if (eta >= 65){
-document.getElementById("prezzo").innerHTML = ("il prezzo del biglietto è di " ) + Math.floor(km * 0.60) + ("€");
-}
+// else if (eta >= 65){
+// document.getElementById("prezzo").innerHTML = ("il prezzo del biglietto è di " ) + Math.floor(km * 0.60) + ("€");
+// }
 
-else if (isNaN(eta) || isNaN(km)){
-  document.getElementById("prezzo").innerHTML = ("inserire i dati corretti Grazie");
-}
-
-else {
-  document.getElementById("prezzo").innerHTML = ("il prezzo del biglietto è di " ) + Math.floor(km) + ("€");
-}
+// else if (!isNaN(nome) || isNaN(km)){
+//   document.getElementById("prezzo").innerHTML = ("inserire i dati corretti Grazie");
+// }
+//
+// else {
+//   document.getElementById("prezzo").innerHTML = ("il prezzo del biglietto è di " ) + Math.floor(km) + ("€");
+// }
